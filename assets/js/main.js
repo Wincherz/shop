6fdb8e9330ad4ea9e2ff166e8f4fb7ea306e5eba
WincherZ <wincher2016@gmail.com>
2019-12-07 19:56:46 +0200

    added js code

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const buttonsContainer = document.querySelector('.page-content');
+const cartCounterLabel = document.querySelector('#cart-counter');
+let cartCounter = 0;
+let cartPrice = 0;
+
+let btnClickHandler = (e) => {
+    const target = e.target;
+
+    if (target.classList.contains('item-actions__cart')) {
+    cartCounterLabel.innerHTML = ++cartCounter;
+
+        if (cartCounter === 1) cartCounterLabel.style.display = 'block';
+
+        const mockData = +target.parentElement.previousElementSibling.innerHTML.replace(/^\$(\d+)\s\D+(\d+).*$/gu, '$1.$2');
+
+        cartPrice = Math.round((cartPrice + mockData)*100)/100;
+        const restoreHTML = target.innerHTML;
+        target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
+        buttonsContainer.removeEventListener('click', btnClickHandler);
+
+        setTimeout(() => {
+            target.innerHTML = restoreHTML;
+            buttonsContainer.addEventListener('click', btnClickHandler);
+        }, 2000);
+    }
+};
+
+buttonsContainer.addEventListener('click', btnClickHandler);
